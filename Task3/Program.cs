7b0d5ef09280c075e9b7e2774ac6d194d938eef2
Dmitriy <trayan@yandex.ru>
2023-07-04 14:07:15 +1100

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] newArray(int[,] oldArray)
+{
+    int [,] newArray = new int [oldArray.GetLength(0), oldArray.GetLength(1)];
+    for(int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            if(i%2 == 1 && j%2 == 1)
+            {
+                newArray[i, j] = oldArray[i, j]*oldArray[i, j];
+            }
+            else
+            {
+                newArray[i, j] = oldArray[i, j];
+            }
+        }
+    }
+    return newArray;
+}
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+int[,] NewArray = newArray(array);
+PrintArray(NewArray);
